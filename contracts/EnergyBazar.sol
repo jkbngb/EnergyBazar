cb7fbc56d3ad026c9efdd0336914851ee18665a4
ic20b026 <ic20b026@technikum-wien.at>
2023-03-20 19:58:15 +0100

    added struct EnergyArticle, mapping, and hashed ID

--- contracts/EnergyBazar.sol
+++ contracts/EnergyBazar.sol
@@ -2,75 +2,84 @@
 pragma solidity ^0.8.16;
 
 contract EnergyBazar {
-  address payable seller;
-  address buyer;
-  string name;
-  uint256 price;
-
-  struct source{
-    bool solar;
-    bool wind;
-    bool hydro;
-    bool fossil;
-    bool other;
+  
+  struct EnergyArticle{
+    bytes32 id;
+    address payable seller;
+    address buyer;
+    string name;
+    uint256 price;
+    bool renewable;
   }
 
-  source energySource;
+  //linking unique article IDs to corresponding EnergyArticle struct
+  mapping(bytes32 => EnergyArticle) public articles;
 
   //Events
-  event ArticleToSell(address indexed _seller, string _name, uint256 _price, source _energySource);
-  event ArticleSold(address indexed _seller, address indexed _buyer, string _name, uint256 _price, source _energySource);
+  event ArticleToSell(bytes32 indexed _id, address indexed _seller, string _name, uint256 _price, bool _renewable);
+  event ArticleSold(bytes32 indexed _id, address indexed _seller, address indexed _buyer, string _name, uint256 _price, bool _renewable);
 
 
   //Sell an article
-  function sellPower(string memory _name, uint256 _price, bool _solar, bool _wind, bool _hydro, bool _fossil, bool _other) public{
-    seller = payable(msg.sender); 
-    name = _name;
-    price = _price;
-
-    energySource = source({
-        solar: _solar,
-        wind: _wind,
-        hydro: _hydro,
-        fossil: _fossil,
-        other: _other
+  function sellPower(string memory _name, uint256 _price, bool _renewable) public {
+    //generation of unique ID
+    bytes32 id = keccak256(abi.encodePacked(msg.sender, _name, _price, _renewable, block.timestamp));
+    
+    //create an EnergyArticle struct with name "article"
+    EnergyArticle memory article = EnergyArticle({
+      id: id,
+      seller: payable(msg.sender),
+      buyer: address(0),
+      name: _name,
+      price: _price,
+      renewable: _renewable
     });
 
-    emit ArticleToSell(seller, name, price, energySource);
+    //stores EnergyArticle struct in the "articles" mapping
+    articles[id] = article;
+
+    //triggers event
+    emit ArticleToSell(article.id, article.seller, article.name, article.price, article.renewable);
   }
 
   //Return article for sale
-  function getArticle() public view returns(
+  function getArticle(bytes32 _id) public view returns(
+    bytes32 id,
     address _seller,
     address _buyer,
     string memory _name,
     uint256 _price,
-    source memory _energySource
+    bool _renewable
   ){
-    return (seller, buyer, name, price, energySource);
+    
+    //retrieves the EnergyArticle struct associated with the input _id from the articles mapping and stores it in a memory variable named article.
+    EnergyArticle memory article = articles[_id]; //updated
+    return (article.id, article.seller, article.buyer, article.name, article.price, article.renewable);
   }
 
   //Buy an article
-    function buyPower() public payable {
+    function buyPower(bytes32 _id) public payable {
+      EnergyArticle storage article = articles[_id];
+
       //is article for sale?
-      require(seller != address(0), "Article not available");
+      require(article.seller != address(0), "Article not available");
       
       //is article sold yet?
-      require(buyer == address(0), "Article already sold");
+      require(article.buyer == address(0), "Article already sold");
 
       //is price correct?
-      require(msg.value == price, "Wrong price");
+      require(msg.value == article.price, "Wrong price");
 
       //Check if renewable energy is used
-      require(energySource.solar || energySource.wind || energySource.hydro, "Non-renewable energy source used");
+      require(article.renewable, "Non-renewable energy source used");
 
       //Sender is recorded as Buyer
-      buyer = msg.sender;
+      article.buyer = msg.sender;
 
       // Transfer the value of the transaction to the seller's address
-      seller.transfer(msg.value);
+      article.seller.transfer(msg.value);
 
       //trigger event
-      emit ArticleSold(seller, buyer, name, price, energySource);
+      emit ArticleSold(article.id, article.seller, article.buyer, article.name, article.price, article.renewable);
   }
 }
